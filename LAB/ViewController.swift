2945438fc541b53f5d17c29b31f04d09b3314149
Kirill Sheremetev <85827294+Rote-cloud@users.noreply.github.com>
2024-03-01 19:20:46 +0600

    Initial Commit

---
+++ LAB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LAB
+//
+//  Created by Kirill Sheremetev on 01.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
